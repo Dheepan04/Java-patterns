698892d6240af4436cb622de30bd03d24934a774
Dheepan <139929478+Dheepan04@users.noreply.github.com>
2023-09-01 22:22:00 +0530

    Add files via upload

--- hill_pattern2.java
+++ hill_pattern2.java
@@ -1,16 +1,16 @@
 package problems;
 
-public class hill_pattern1 {
+public class hill_pattern2 {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
                 System.out.print("  ");
             }
-            for (int j = 1; j <= i; j++) {
+            for ( int j = i; j <=n; j++) {
                 System.out.print("* ");
             }
-            for (int j = 1; j < i; j++) {  //note less than
+            for (int j = i; j < n; j++) {  //note less than
                 System.out.print("* ");
             }
             System.out.println();
